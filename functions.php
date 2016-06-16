e89375b25ac08b7a9b2b7743572c5d5c895f1f5c
Eli <eliscolatour@gmail.com>
2016-06-16 23:52:04 +0100

    Ajout du header de wordpress, ainsi qu'un menu de navigation spécifiquement pour le thème Enfant.

--- functions.php
+++ functions.php
@@ -1,11 +1,76 @@
 <?php
 /*
- * Description: Fichier Functions du thème enfant 
- * Author: Eli
+ * =============================================
+ * functions.php
  * 
+ * le fichier functions du thème enfant
+ * 
+ * Description: Fichier Functions du thème enfant 
+ * 
+ * Author: Eli
+ * =============================================
  */
 
+
+
+
+
+
+/*
+ * ---------------------------------------------
+ * 1- LES CONSTANTES
+ * ---------------------------------------------
+ */
+
+// Répertoire du fichier css du thème
+ define('THEMEROOT', get_stylesheet_directory_uri());
+ 
+// Répertoire des images
+define('IMAGES', THEMEROOT.'/images');
+
+// Répertoire des images
+define('JS', THEMEROOT.'/js');
+
+
+
+
+
+
+/*
+ * ---------------------------------------------
+ * 2- CONFIGURATION DU THEME
+ * ---------------------------------------------
+ */
+
+if(!function_exists('sixteen_child_setup')){
+    function sixteen_child_setup(){
+        
+        // Autoriser la traduction de ce thème
+        $lang_dir = THEMEROOT.'/languages';
+        load_child_theme_textdomain('mavor',$lang_dir);
+        
+        //Ajout du support pour les liens des flux automatiques
+        add_theme_support('auutomatic-feed-links');
+        
+        //Ajout du support des vignettes des images
+        add_theme_support('posts-thumbnails');
+        
+        //Enregistrement des menus de navigation
+        register_nav_menus( array(
+            'enfant-menu-principal'=>  __('PRINCIPAL MENU ENFANT','mavor')
+        ));
+    }
+    
+    //Application de la configuration définie
+    add_action('after_setup_theme','sixteen_child_setup');
+}
+
+
+
+
+/*
 add_action("wp_enqueue_scripts","theme_enqueue_styles");
 function theme_enqueue_styles(){
     wp_enqueue_style(get_template_directory_uri().STYLESHEETPATH);
 }
+*/
--- header.php
+++ header.php
@@ -1,8 +1,63 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+<?php //
+/*
+ * ============================
+ * header.php
+ * 
+ * 
+ * The theme Header
+ * ============================
  */
+?>
 
+<!DOCTYPE>
+<html <?php language_attributes(); ?> class="no-js">
+
+    <head>
+        <meta charset="<?php bloginfo("chaset"); ?>"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <title><?php wp_title("|", TRUE, "right") ?></title>
+        <meta name="description" content="<?php bloginfo('description'); ?>" />
+        <meta name="viewport" content="device=device-width, initial-scale=1"/>
+
+        <!--Le favicon et l'Apple Touch icon-->
+        <?php
+           $favicon = IMAGES . '/icons/favicon.ico';
+           $touch_icon = IMAGES . '/icons/apple-touch-icon-precomposed.png';
+        ?>
+        <link rel="shortcut icon" href="<?php echo $favicon; ?>" />
+        <link rel="apple-touch-icon-precomposed" href="<?php echo $touch_icon; ?>" sizes="152x152"/>
+        
+        <!--Charger les metas de Wordpress-->
+        <?php wp_head(); ?>
+        
+    </head>
+    <body <?php body_class(); //Ajout de toutes les classes nécessaires Wordpress à la balise body?>>
+            <div class="site-wrapper">
+
+                <div class="site-wrapper-inner">
+
+                    <div class="cover-container">
+
+                        <div class="masthead clearfix">
+                            <div class="inner">
+                                <h3 class="masthead-brand">
+                                    <a href="<?php esc_url(home_url('/')); ?>">
+                                        <?php bloginfo('name'); ?>
+                                    </a>
+                                </h3>
+                                <nav>
+                                    <!--<ul class="nav masthead-nav">
+                                        <li class="active"><a href="#">Home</a></li>
+                                        <li><a href="#">Features</a></li>
+                                        <li><a href="#">Contact</a></li>
+                                    </ul>-->
+                                    <?php
+                                            wp_nav_menu( array(
+                                                'menu_class'=>'nav masthead-nav' ,
+                                                'theme_location'=>'enfant-menu-principal',
+                                                 'container'=>FALSE
+                                            ));
+                                    ?>
+                                </nav>
+                            </div>
+                        </div>
